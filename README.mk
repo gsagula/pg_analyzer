92b3ad0d34ab9188c82b91d5d15b6a6a45b0d0c2
Gabe <gabe@Gabes-MacBook-Pro.local>
2021-03-18 12:45:15 -0700

    added README
    Signed-off-by: Gabe <gabe@Gabes-MacBook-Pro.local>

---
+++ README.mk
@@ -0,0 +1,11 @@
+### PG Query Analyzer
+
+CGO library usage example based on native Postgres backend `raw_parser`.
+
+# Running
+```
+$ go build main.go && ./main
+```
+
+# Notes
+First time may take a few minutes for building the C library.
